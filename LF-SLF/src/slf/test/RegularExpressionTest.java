43d316a1d9fd4e6bd7e2a7dc27b88099b934299e
Lucas Petry <lucasmaypetry@gmail.com>
2015-04-27 19:46:30 -0300

    Classe de teste para expressão regular criada.

---
+++ LF-SLF/src/slf/test/RegularExpressionTest.java
@@ -0,0 +1,19 @@
+package slf.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegularExpressionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
