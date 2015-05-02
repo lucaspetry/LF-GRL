cdd45416291c025debbd13b02e3f5df3616bd933
Lucas Petry <lucasmaypetry@gmail.com>
2015-05-01 21:06:06 -0300

    Classe State incrementada com atributos básicos.

--- LF-SLF/src/slf/automaton/State.java
+++ LF-SLF/src/slf/automaton/State.java
@@ -5,5 +5,34 @@ package slf.automaton;
  * @author lucas
  */
 public class State {
+	
+	/**
+	 * Construtor.
+	 * @param name nome do estado.
+	 * @param isFinal true caso o estado seja final.
+	 */
+	public State(String name, boolean isFinal) {
+		this.name = name;
+		this.isFinal = isFinal;
+	}
+	
+	/**
+	 * Obter o nome do estado.
+	 * @return nome do estado.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Verificar se o estado é final.
+	 * @return true caso o estado seja final.
+	 */
+	public boolean isFinal() {
+		return isFinal;
+	}
+
+	private String name;
+	private boolean isFinal;
 
 }
